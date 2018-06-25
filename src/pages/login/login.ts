484796245d9ee854f993adc27c9c497ed204000d
Simon Tod <simon.todeschini@viacesi.fr>
2018-06-25 22:32:29 +0200

    Added android device & started Legrand Oauth2 implementation

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -11,6 +11,9 @@ import { EliotAPIProvider } from '../../providers/EliotAPI';
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
+
+declare var window: any;
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
@@ -31,6 +34,14 @@ export class LoginPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad loginPage');
+  }
+
+  ionViewDidEnter() {
+    this.menu.swipeEnable(false);
+  }
+
+  ionViewWillLeave() {
+    this.menu.swipeEnable(true);
   }
 
   loginForm() {
@@ -62,12 +73,39 @@ export class LoginPage {
     );
   }
 
-  ionViewDidEnter() {
-    this.menu.swipeEnable(false);
+  login() {
+    this.platform.ready().then(() => {
+      this.legrandLogin().then(success => {
+          alert(success.code);
+      }, (error) => {
+          alert(error);
+      });
+    });
   }
 
-  ionViewWillLeave() {
-    this.menu.swipeEnable(true);
+  legrandLogin(): Promise<any> {
+    return new Promise(function(resolve, reject) {
+      var browserRef = window.cordova.InAppBrowser.open("https://partners-login.eliotbylegrand.com/authorize?client_id=" + "9999d816-d539-463a-afbd-7fc663de1c8a" + "&redirect_uri=" + "https://login.microsoftonline.com/tfp/oauth2/nativeclient" + "&response_type=code", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+      browserRef.addEventListener("loadstart", (event) => {
+          if ((event.url).indexOf("https://login.microsoftonline.com/tfp/oauth2/nativeclient") === 0) {
+              browserRef.removeEventListener("exit", (event) => {});
+              browserRef.close();
+              var responseParameters = ((event.url).split("?")[1]).split("&");
+              var parsedResponse = {};
+              for (var i = 0; i < responseParameters.length; i++) {
+                  parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+              }
+              if (parsedResponse["code"] !== undefined && parsedResponse["code"] !== null) {
+                  resolve(parsedResponse);
+              } else {
+                  reject("Problem authenticating with Legrand");
+              }
+          }
+      });
+      // browserRef.addEventListener("exit", function(event) {
+      //     reject("The Legrand sign in flow was canceled");
+      // });
+  });
   }
 
 }
