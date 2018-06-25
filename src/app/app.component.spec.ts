58a94321bd75905952ef09a1ed69425379d34c87
Simon Tod <simon.todeschini@viacesi.fr>
2018-06-25 19:22:40 +0200

    Modified src/app/app.component.spec.ts to work with restored application

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { NO_ERRORS_SCHEMA, EventEmitter } from '@angular/core';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { MyApp } from './app.component';
+
+import { HttpRequestsProvider } from '../providers/HttpRequests';
 
 describe('MyApp', () => {
 
@@ -12,13 +14,39 @@ describe('MyApp', () => {
   let instance: MyApp;
 
   const platformStub = {
-    ready: (): Promise<string> => new Promise<string>((resolve, reject) => resolve('ready'))
+    ready: (): Promise<string> => new Promise<string>((resolve, reject) => resolve('ready')),
+    resume: {
+      subscribe: (): void => undefined
+    }
   };
   const statusBarStub = {
     styleDefault: (): void => undefined
   };
   const splashScreenStub = {
     hide: (): void => undefined
+  };
+  const menuControllerStub = {
+    close: (): Promise<Boolean> => new Promise<Boolean>((resolve, reject) => resolve(true))
+  };
+  // const alertsProviderStub = {
+  //   showErrorAlert: (): void => undefined,
+  //   showSuccessAlert: (): void => undefined
+  // };
+  const httpRequestProviderStub = {
+    // post: (): Promise<any> => undefined,
+    // get: (): Promise<any> => undefined,
+    checkUserLoggedIn: (): Promise<Boolean> => new Promise<Boolean>((resolve, reject) => resolve(true)),
+    // saveToken: (): void => undefined,
+    // getTokenFromStorage: (): Promise<any> => undefined,
+    clearStorage: (): void => undefined
+  };
+  const eliotAPIProviderStub = {
+    authenticate: (): Promise<string> => undefined
+  };
+  const mobileAPIProviderStub = {
+    getDevices: (): Promise<Array<any>> => undefined,
+    getDeviceInfo: (): Promise<any> => undefined,
+    sendCommandToDevice: (): Promise<any> => undefined
   };
 
   beforeEach(() => {
@@ -32,7 +60,9 @@ describe('MyApp', () => {
       providers: [
         {provide: Platform, useValue: platformStub},
         {provide: StatusBar, useValue: statusBarStub},
-        {provide: SplashScreen, useValue: splashScreenStub}
+        {provide: SplashScreen, useValue: splashScreenStub},
+        {provide: MenuController, useValue: menuControllerStub},
+        {provide: HttpRequestsProvider, useValue: httpRequestProviderStub},
       ]
     }).compileComponents();
   });
