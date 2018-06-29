333cbc64c018ae7da86f3baac134ae3df0fcd0c1
Simon Tod <simon.todeschini@viacesi.fr>
2018-06-29 14:29:19 +0200

    Device page connected to MobileApi

--- src/pages/device/device.ts
+++ src/pages/device/device.ts
@@ -15,6 +15,9 @@ import { SendCommandToDevicePage } from '../sendCommandToDevice/sendCommandToDev
     templateUrl: 'device.html'
 })
 export class DevicePage {
+  metricType: string = "calcMetrics";
+  loadingCalcMetrics: boolean;
+  loadingRawMetrics: boolean;
   public device: any;
 
   constructor(
@@ -26,27 +29,59 @@ export class DevicePage {
     public mobileApi: MobileAPIProvider
   ) {
     this.device = this.navParams.get('device');
+    this.device.rawMetrics = [];
+    this.device.calcMetrics = [];
   }
 
     ionViewDidLoad() {
       console.log('ionViewDidLoad devicePage');
 
-      let loader = this.loadingCtrl.create({
-        content: "Loading Devices List"
-      });
-      //Show the loading indicator
-      loader.present();
+      this.getRawMetrics();
+      this.getCalcMetrics();
+    }
 
-      this.mobileApi.getDeviceInfo(this.device).then(
+    getRawMetrics(infiniteScroll?) {
+      if (!infiniteScroll) this.loadingRawMetrics = true;
+      var timestamp: number = Date.now();
+      if (this.device.rawMetrics.length != 0)
+        timestamp = this.device.rawMetrics[this.device.rawMetrics.length-1].date.getTime();
+        
+      this.mobileApi.getDeviceRawMetrics(this.device, timestamp).then(
         data => {
-          loader.dismiss();
-          this.device = data;
+          if (infiniteScroll) infiniteScroll.complete();
+          else this.loadingRawMetrics = false;
+          this.device.rawMetrics = this.device.rawMetrics.concat(data);
+          if (data.length == length && infiniteScroll) infiniteScroll.enable(false);
         },
         error => {
-          loader.dismiss();
-          console.error('Error getDeviceInfo');
+          if (infiniteScroll) infiniteScroll.complete();
+          else this.loadingRawMetrics = false;
+          console.error('Error getDeviceRawMetrics');
           console.dir(error);
-          this.alerts.showErrorAlert(error, "getDeviceInfo");
+          this.alerts.showErrorAlert(error, "getDeviceRawMetrics");
+        }
+      )
+    }
+
+    getCalcMetrics(infiniteScroll?) {
+      if (!infiniteScroll) this.loadingCalcMetrics = true;
+      var timestamp: number = Date.now();
+      if (this.device.calcMetrics.length != 0)
+        timestamp = this.device.calcMetrics[this.device.calcMetrics.length-1].dateTimeEnd.getTime();
+
+      this.mobileApi.getDeviceCalcMetrics(this.device, timestamp).then(
+        data => {
+          if (infiniteScroll) infiniteScroll.complete();
+          else this.loadingCalcMetrics = false;
+          this.device.calcMetrics = this.device.calcMetrics.concat(data);
+          if (data.length == length && infiniteScroll) infiniteScroll.enable(false);
+        },
+        error => {
+          if (infiniteScroll) infiniteScroll.complete();
+          else this.loadingCalcMetrics = false;
+          console.error('Error getDeviceCalcMetrics');
+          console.dir(error);
+          this.alerts.showErrorAlert(error, "getDeviceCalcMetrics");
         }
       )
     }
